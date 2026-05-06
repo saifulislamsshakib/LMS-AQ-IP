3732b3c54eedc17d0fe7d1f71980382332ace8d0
Saiful Islam <saifulislamsshakib@gmail.com>
2026-05-06 18:43:13 +0600

    added vercel domain to cors

--- server/index.js
+++ server/index.js
@@ -37,6 +37,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "https://lms-aq-ip.vercel.app",
+      "https://lms-aq-alobrn60b-saifulislamsshakibs-projects.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
